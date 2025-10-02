bda1e63556212c4d78eae6c7f3d4a0ab17ea9f24
shivam <shivamkosti570@gmail.com>
2025-10-02 13:42:36 +0530

    new api created

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get("/api/get", (req, res) => {
     res.send({message: "Nodejs AWS Deployment"});
 })
 
+app.get("/api/user", (req, res) => {
+    res.send({name: "shivam", email: "shivam@gmail.com", role: "Developer"})
+})
 app.listen(PORT, (req,res) => {
     console.log(`server is running on port ${PORT}`)
 })
